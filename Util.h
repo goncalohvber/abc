4e9322cd9959e2c8bfa8008aa914f84001c4894c
Imper4triz <ax.isabela@gmail.com>
2025-12-26 15:52:38 -0300

    Create Util.h

---
+++ Util.h
@@ -0,0 +1,13 @@
+//
+//  Util.h
+//  ProjetoEstacionamento
+//
+//  Created by Gonçalo Henrique Viegas Bernardino on 11/12/2025.
+//
+
+#ifndef Util_h
+#define Util_h
+
+#include <stdio.h>
+void mostrarmenu(void);
+#endif /* Util_h */
