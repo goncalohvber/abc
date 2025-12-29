b4a6e9a231980238a7413a02c0161ae73004a521
goncalohvber <goncalohvber@gmail.com>
2025-12-29 01:53:36 +0000

    Update Estacionamento.h

--- Estacionamento.h
+++ Estacionamento.h
@@ -1,6 +1,7 @@
 #ifndef Estacionamento_h
 #define Estacionamento_h
 #include "Instalacao.h"
+#include "Tarifas.h"
 #include <stdio.h>
 
 typedef struct estacionamento{
@@ -10,8 +11,16 @@ typedef struct estacionamento{
     char lugar[6];
 } estacionamento;
 
+typedef struct {
+    char status; // 'X' = ocupado, '-' = livre
+    char matricula[10]; // guardar matrícula do carro
+} Lugar;
+
 void MostrarFicheiroEstacionamento(char *nomeFicheiro);
 int personalizapp(Confparque config);
+void MostrarMapaOcupacao_ComMapa(Confparque config, char *ficheiroOcupacao, Lugar mapa[][MAX_FILAS][MAX_LUGARES]);
+void InicializarMapa(Lugar mapa[][MAX_FILAS][MAX_LUGARES], Confparque config);
+void PreencherMapaComOcupacoes(Lugar mapa[][MAX_FILAS][MAX_LUGARES], char *ficheiroOcupacao);
+void DesenharMapa(Lugar mapa[][MAX_FILAS][MAX_LUGARES], Confparque config);
 
 #endif /* Estacionamento_h */
-
